eeb109f25b48741e9dc5cb98d6572e53e248575d
satyam prajapati <satyamprajapati1979@gmail.com>
2025-02-15 12:26:50 +0530

    making defualt gpt genarted

--- server.js
+++ server.js
@@ -5,17 +5,17 @@ const path = require('path');
 const send_mail = require('./routes/send_mail_router');
 const cors = require('cors')
 
-
+// index is deepseek genrated whiel index2 is genarted by gpt
 app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+  res.sendFile(path.join(__dirname, 'public', 'index2.html'))  
 }) 
 
 app.get('/gpt', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'gpt.html'))
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
 app.use('/mail', send_mail)
