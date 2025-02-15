4ef734d9c179ca3d5cb4efe1995eac69461538e6
satyam prajapati <satyamprajapati1979@gmail.com>
2025-02-15 18:54:15 +0530

    adding cors

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'))  
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
 }) 
 
 app.get('/deepseek', (req, res) => {
